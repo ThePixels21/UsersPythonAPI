86b475f4d0c052e534eb05062498aaf0ab4e49fc
ThePixels21 <santigamer521@gmail.com>
2024-10-03 22:27:30 -0500

    pylint fixed

--- FastAPI/app/database.py
+++ FastAPI/app/database.py
@@ -2,4 +2,3 @@
 This module configures the MySQL database connection and defines
 the Peewee model for interacting with the 'employees' table.
 """
-
--- FastAPI/app/main.py
+++ FastAPI/app/main.py
@@ -4,4 +4,3 @@ Main module for FastAPI application setup.
 This module sets up the FastAPI application, manages the database connection
 lifecycle, and includes routes.
 """
-
